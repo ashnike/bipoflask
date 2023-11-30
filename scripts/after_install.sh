b0f540d62490749f0ce3b1f1174b1bf61a500728
Ashnil <85815731+ashnike@users.noreply.github.com>
2023-11-30 22:45:01 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Copy static files to the deployment directory
-cd /home/ubuntu/bipoflask
+cd /home/ubuntu/app
 pip install -r requirements.txt
 # Optionally, set appropriate permissions or configurations for serving static content
