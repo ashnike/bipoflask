f0ec0a4e1e357f336c052b7387e1ed9fac93f7f0
Ashnil <85815731+ashnike@users.noreply.github.com>
2023-11-30 23:19:54 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,4 +3,5 @@
 # Change directory to the location of static files and start a basic HTTP server
 cd /home/ubuntu/app # Change this to your directory containing static files
 #python -m http.server 8000 &  # Start a simple HTTP server on port 8000 (adjust as needed)
-python app.py &
+gunicorn -w 2 -b 127.0.0.1:5000 app:app
+
