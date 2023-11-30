08923dfbdf7e45e31e31ee0af17872aa7ad803a5
Ashnil <85815731+ashnike@users.noreply.github.com>
2023-11-30 22:44:40 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Change directory to the location of static files and start a basic HTTP server
-cd /home/ubuntu/bipoflask # Change this to your directory containing static files
-python -m http.server 8000 &  # Start a simple HTTP server on port 8000 (adjust as needed)
+cd /home/ubuntu/app # Change this to your directory containing static files
+#python -m http.server 8000 &  # Start a simple HTTP server on port 8000 (adjust as needed)
+python app.py &
