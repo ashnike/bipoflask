da7b2ec659e8d855ef8996c829a216b49a9cf118
Ashnil <85815731+ashnike@users.noreply.github.com>
2023-11-30 22:42:43 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
 # Update the package manager and install required system dependencies
+cd /home/ubuntu/app
 apt-get update
 apt-get install -y python3-pip python3-venv
+
+apt-get install -y nginx
+
+# Remove existing contents of the NGINX default file
+sudo truncate -s 0 /etc/nginx/sites-available/default
+
+# Write the new configuration to the NGINX default file
+cat <<EOL | sudo tee /etc/nginx/sites-available/default
+server {
+    listen 80;
+    server_name your-domain.com;  # Replace with your actual domain
+
+    location / {
+        proxy_pass http://localhost:5000;  # Replace with your app's address
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        # Additional proxy_set_header directives if needed
+    }
+}
+EOL
+
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+
+# Verify the NGINX configuration
+sudo nginx -t
+
+# Restart NGINX to apply the new configuration
+sudo systemctl restart nginx
