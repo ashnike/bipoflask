526e3a33fdfe137354af4cbe5f4e9ac61d6b3bd0
Ashnil <85815731+ashnike@users.noreply.github.com>
2023-12-01 07:11:36 +0530

    Update app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,6 +2,7 @@
 
 # Change directory to the location of static files and start a basic HTTP server
 cd /home/ubuntu/app # Change this to your directory containing static files
-gunicorn -w 1 -b 127.0.0.1:5000 app:app
+exec &>> /home/ubuntu/app/gunicorn.log
+gunicorn -w 1 -b 127.0.0.1:5000 app:app >> /home/ubuntu/app/gunicorn.log 2>&1 &
 
 
