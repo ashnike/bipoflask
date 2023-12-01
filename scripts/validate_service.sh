09ff4f51385118030d721c1369d8c857f2761338
Ashnil <85815731+ashnike@users.noreply.github.com>
2023-12-01 07:26:21 +0530

    Update validate_service.sh

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/app
-# Example: Check if Flask app is running
-if pgrep -f "python app.py" > /dev/null
+
+
+# Example: Check if Flask app is running with Gunicorn
+if pgrep -f "gunicorn -w 1 -b 127.0.0.1:5000 app:app" > /dev/null
 then
-    echo "Flask application is running."
+    echo "Flask application is running with Gunicorn."
     exit 0
 else
-    echo "Flask application is not running."
+    echo "Flask application is not running with Gunicorn."
     exit 1
 fi
+
